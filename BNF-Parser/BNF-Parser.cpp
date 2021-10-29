d296f8c363bd303edb317d2bc19902a91c34977d
thewebrage <ein.nuff@gmail.com>
2021-10-28 22:06:36 -0600

    More work on main function

--- BNF-Parser/BNF-Parser.cpp
+++ BNF-Parser/BNF-Parser.cpp
@@ -14,23 +14,40 @@ using std::map;
 string nonTerminals[] = { "GOAL", "EXPR", "EXPRp", "TERM", "TERMp", "FACTOR" };
 string terminals[] = { "eof", "+", "-", "*", "/", "(", ")", "name", "num" };
 
+vector<string> readInFile(string filePath) {
+	vector<string> lines;
+	std::ifstream file;
+	file.open(filePath, std::ios::out);
+	if (!file) {
+		// File coun't open
+		std::cout << "Couldn't file file " << filePath;
+	}
+	else {
+		string line;
+		while (getline(file, line))
+			lines.push_back(line);
+
+		file.close();
+	}
+	return lines;
+}
 
 vector<vector<string>> createProductionTable() {
 	vector<vector<string>> productionTable;
 
 	// Insert productions into structure
-	productionTable[0] = { "EXPR" };
-	productionTable[1] = { "TERM", "EXPRp" };
-	productionTable[2] = { "+", "TERM", "EXPRp" };
-	productionTable[3] = { "-", "TERM", "EXPRp" };
-	productionTable[4] = { "e" };
-	productionTable[5] = { "FACTOR", "TERMp" };
-	productionTable[6] = { "*", "FACTOR", "TERMp" };
-	productionTable[7] = { "/", "FACTOR", "TERMp" };
-	productionTable[8] = { "e" };
-	productionTable[9] = { "(", "EXPR", ")" };
-	productionTable[10] = { "num" };
-	productionTable[11] = { "name" };
+	productionTable.push_back({ "EXPR" });
+	productionTable.push_back({ "TERM", "EXPRp" });
+	productionTable.push_back({ "+", "TERM", "EXPRp" });
+	productionTable.push_back({ "-", "TERM", "EXPRp" });
+	productionTable.push_back({ "e" });
+	productionTable.push_back({ "FACTOR", "TERMp" });
+	productionTable.push_back({ "*", "FACTOR", "TERMp" });
+	productionTable.push_back({ "/", "FACTOR", "TERMp" });
+	productionTable.push_back({ "e" });
+	productionTable.push_back({ "(", "EXPR", ")" });
+	productionTable.push_back({ "num" });
+	productionTable.push_back({ "name" });
 
 	return productionTable;
 }
@@ -84,33 +101,66 @@ map<string, map<string, int>> createParseTable() {
 	return parseTable;
 }
 
-string nextWord() {
-	// TODO: add functionality here
-	return "";
+string getFocus(string word) {
+	if (!(std::find(std::begin(terminals), std::end(terminals), word) != std::end(terminals))) {
+		if (std::isdigit(word[0]))
+			return "num";
+		else
+			return "name";
+	}
+
+	return word;
 }
 
-bool checkLine(vector<vector<string>> productionTable, map<string, map<string, int>> parseTable) {
+string nextWord(string& line) {
+	char keyTerms[] = { '+', '-', '*', '/', '(', ')' };
+	string word = "";
+
+	if (line.size() > 0) {
+		word += line[0];
+
+		for (int i = 0; i < line.length(); i++) {
+			for (char term : keyTerms) {
+				if (line[i] == term || line[i] == ' ') {
+					break;
+				}
+			}
+
+			word += line[i];
+		}
+	}
+
+	line.erase(0, word.size());
+
+	if (word == "")
+		word = "eof";
+
+	return getFocus(word);
+}
+
+int checkLine(vector<vector<string>> productionTable, map<string, map<string, int>> parseTable, string line) {
 	vector<string> stack;
 	string focus;
 
-	string word = nextWord();
+	string word = nextWord(line);
 	stack.push_back("eof");
 	stack.push_back("S");
-	focus = stack.front();
+	focus = stack.back();
+
 
 	while (true) {
 		if (focus == "eof" && word == "eof") {
 			// then report successand exit the loop;
-			return true;
+			return 1;
 		}
 		else if ((std::find(std::begin(terminals), std::end(terminals), focus) != std::end(terminals)) || focus == "eof") {
 			if (focus == word) {
 				stack.pop_back();
-				word = nextWord();
+				word = nextWord(line);
 			}
 			else {
 				// else report an error looking for symbol at top of stack;
-				return false;
+				return 0;
 			}
 		}
 		else {
@@ -129,26 +179,31 @@ bool checkLine(vector<vector<string>> productionTable, map<string, map<string, i
 			}
 			else {
 				// report an error expanding focus;
-				return false;
+				return 0;
 			}
 		}
-		focus = stack.front();
+		focus = stack.back();
 	}
 }
 
 int main()
 {
 	// Read in files 
-	// TODO: read in files
+	vector<string> file = readInFile("./valid.txt");
 
 	// Create structures for the algorithm
 	vector<vector<string>> productionTable = createProductionTable();
 	map<string, map<string, int>> parseTable = createParseTable();
 
-	// TODO: for each line in file
-	// Perform the algorithm
-	checkLine(productionTable, parseTable);
-	// TODO: print result to user;
+	for (string line : file) {
+		string passedStr = "invalid";
+
+		// Perform the algorithm
+		if (checkLine(productionTable, parseTable, line))
+			passedStr = "valid";
+
+		std::cout << "(" << passedStr << "): " << line << "\n";
+	}
 
 	return 0;
 }
